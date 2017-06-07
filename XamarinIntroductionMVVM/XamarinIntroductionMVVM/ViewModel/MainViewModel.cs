ca8a497fdf52ffa93ffaae41245e5297dffca198
diegoalexan <diegoalexan@yahoo.com.ar>
2017-06-06 23:30:19 -0500

    Carga Inicial De la solucion

---
+++ XamarinIntroductionMVVM/XamarinIntroductionMVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace XamarinIntroductionMVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        public decimal Pesos { get; set; }
+
+        public ICommand ConvertCommand { get { return new RelayCommand(ConvertMoney); } }
+
+        private void ConvertMoney()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
